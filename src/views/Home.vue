d1b73a08472bd12c2bddb24b0b8ab88a08ab3959
IamBani <13972186114.com>
2021-12-07 09:37:27 +0800

    新增a测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <div>测试</div>
+    <div>{{a}}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
+  },
+  setup () {
+    const a = ref(0)
+    return {
+      a
+    }
   }
 })
 </script>
